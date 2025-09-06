48f201df52766cb69ac3db618c5f466d7f7c9de0
lelepton <leticyammedeiros@gmail.com>
2025-09-06 07:25:02 -0300

    feat: adiciona botão para trocar o idioma (pt-BR ou en-US)

---
+++ TeamUP_Front/components/MudarLingua.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Text, TouchableOpacity, StyleSheet } from 'react-native';
+import { useTranslation } from 'react-i18next';
+import { Cores } from '../constants/Cores';
+
+export default function MudarLingua() {
+    const { i18n } = useTranslation();
+
+    return (
+        <TouchableOpacity onPress={() => i18n.changeLanguage(i18n.language === 'pt-BR' ? 'en-US' : 'pt-BR')} style={styles.button}>
+            <Text style={styles.buttonText}>{i18n.language === 'pt-BR' ? 'EN' : 'PT'}</Text>
+        </TouchableOpacity>
+    );
+}
+
+const styles = StyleSheet.create({
+    button: {
+        backgroundColor: Cores.verde,
+        padding: 5,
+        borderRadius: 5,
+        alignSelf: 'flex-end',
+        right: '5%',
+    },
+    buttonText: {
+        color: Cores.azul,
+        fontSize: 12,
+    },
+});
--- TeamUP_Front/pages/PaginaDeLogin.jsx
+++ TeamUP_Front/pages/PaginaDeLogin.jsx
@@ -1,5 +1,6 @@
 import { View, StyleSheet, Image, Dimensions } from "react-native";
 import Login from "../components/Login";
+import MudarLingua from "../components/MudarLingua";
 import { Cores } from "@/constants/Cores";
 
 const { width, height } = Dimensions.get('window');
@@ -7,23 +8,30 @@ const { width, height } = Dimensions.get('window');
 export default function PaginaDeLogin() {
   return (
     <View style={styles.container}>
-        <Image source={require('../assets/images/logo.png')} style={styles.logo} resizeMode="contain" />
-        <Login />
+      <MudarLingua style={styles.mudarLingua} />
+      <Image source={require('../assets/images/logo.png')} style={styles.logo} resizeMode="contain" />
+      <Login />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    width: '100%',
+    height: '100%',
     flex: 1,
     backgroundColor: Cores.branco,
     justifyContent: 'center',
     alignItems: 'center',
+    position: 'relative',
   },
   logo: {
     width: width * 0.5,
     aspectRatio: 1,
     maxHeight: height * 0.3,
     marginBottom: 50,
+  },
+  mudarLingua: {
+    position: 'absolute',
   },
 });
