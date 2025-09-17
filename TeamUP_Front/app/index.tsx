8eb1cfe6e1e5c5e32013e07f1e5372842b29a0aa
lelepton <leticyammedeiros@gmail.com>
2025-09-17 16:56:25 -0300

    feat: adiciona componente do cadastro (ainda incompleto)

--- TeamUP_Front/app/index.tsx
+++ TeamUP_Front/app/index.tsx
@@ -1,10 +1,12 @@
 import { View } from "react-native";
-import PaginaDeLogin from "../pages/PaginaDeLogin";
+// import PaginaDeLogin from "../pages/PaginaDeLogin";
+import PaginaDeCadastro from "../pages/PaginaDeCadastro";
 
 export default function Index() {
   return (
     <View style={{ flex: 1 }}>
-      <PaginaDeLogin />
+      {/* <PaginaDeLogin /> */}
+      <PaginaDeCadastro />
     </View>
   );
 }
---
+++ TeamUP_Front/pages/PaginaDeCadastro.jsx
@@ -0,0 +1,59 @@
+import { View, StyleSheet, Image } from "react-native";
+import Cadastro from "../components/Cadastro";
+import MudarLingua from "../components/MudarLingua";
+import MudarTema from "../components/MudarTema";
+import { useTheme } from '../contexts/ThemeContext';
+
+export default function PaginaDeCadastro() {
+  const { temaAtual } = useTheme();
+
+  return (
+    <View style={[styles.container, { backgroundColor: temaAtual.fundo }]}>
+      <View style={styles.botoes}>
+        <MudarLingua />
+        <MudarTema />
+      </View>
+      <View style={styles.logo}>
+        <Image 
+          source={require('../assets/images/logo.png')} 
+          style={styles.logoImg} 
+          resizeMode="contain" 
+        />
+      </View>
+      <View style={styles.login}>
+        <Cadastro />
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    position: 'relative',
+  },
+  botoes: {
+    position: 'absolute',
+    top: 40,
+    right: 40,
+    zIndex: 10,
+  },
+  logo: {
+    flex: 4,
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '80%',
+  },
+  logoImg: {
+    width: '100%',
+    height: '100%',
+  },
+  login: {
+    flex: 6,
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '100%',
+  }
+});
