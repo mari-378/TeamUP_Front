80467d99aa941c76ceef755b4f16af408455cbb2
lelepton <leticyammedeiros@gmail.com>
2025-09-24 13:13:54 -0300

    feat: adiciona prop para receber nome do time no componente do CardPlacar

--- TeamUP_Front/components/CardPlacar.jsx
+++ TeamUP_Front/components/CardPlacar.jsx
@@ -3,25 +3,36 @@ import { View, StyleSheet, Text } from 'react-native';
 import { useTheme } from '../contexts/ThemeContext';
 import { Cores } from '../constants/Cores';
 
-export default function CardPlacar( {pontuacao} ) {
+export default function CardPlacar( {time, pontuacao} ) {
     const { temaAtual } = useTheme();
 
     return (
-        <View style={[styles.container, { backgroundColor: temaAtual.caixaTexto }]}>
-            <Text style={styles.textoContainer}>{pontuacao}</Text>
-        </View>
+        <View style={styles.container}>
+            <Text style={styles.textoTime}>{time}</Text>
+            <View style={[styles.placar, { backgroundColor: temaAtual.caixaTexto }]}>
+                <Text style={styles.textoContainer}>{pontuacao}</Text>
+            </View>
+        </View> 
     )
 };
 
 const styles = StyleSheet.create({
     container: {
-        width: '20%',
+        width: '25%',
+        height: '20%',
         maxWidth: 350,
-        height: '15%',
         maxHeight: '200',
+    },
+    textoTime: {
+        lineHeight: 1.6,
+    },
+    placar: {
+        width: '90%',    
+        height: '75%',
         justifyContent: 'center',
         alignItems: 'center',
         borderRadius: 20,
+        margin: 10,
     },
     textoContainer: {
         fontSize: 40,
