be4d542cdcf99b88c835c07df4a058de0afc64d6
lelepton <leticyammedeiros@gmail.com>
2025-10-02 02:42:13 -0300

    feat: adiciona logo na pagina de selecionar esporte para sorteio

--- TeamUP_Front/components/SelecaoMaxJogadores.jsx
+++ TeamUP_Front/components/SelecaoMaxJogadores.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, Image, Text } from 'react-native';
 import { useTranslation } from 'react-i18next';
 import { useRouter } from 'expo-router';
+import { useTheme } from '../contexts/ThemeContext';
 import Botao from './Botao';
 
 export default function SelecaoMaxJogadores() {
   const { t } = useTranslation();
   const router = useRouter();
+  const { temaAtual } = useTheme();
 
-  // Define o máximo de jogadores por time de acordo com o esporte
   const maxPorTime = {
     football: 11,
     volleyball: 6,
@@ -23,7 +24,15 @@ export default function SelecaoMaxJogadores() {
   };
 
   return (
-    <View style={styles.container}>
+    <View style={[styles.container, { backgroundColor: temaAtual.fundo }]}>
+      <Text style={[styles.texto, { color: temaAtual.texto }]}>{t('selectSport.title')}</Text>
+      <View style={styles.logo}>
+        <Image 
+          source={require('../assets/images/logo.png')} 
+          style={styles.logoImg} 
+          resizeMode="contain" 
+        />
+      </View>
       <Botao
         title={t('sports.football')} 
         onPress={() => navegarParaSorteio('football')}
@@ -43,5 +52,19 @@ export default function SelecaoMaxJogadores() {
 const styles = StyleSheet.create({
   container: {
     gap: 20,
+    height: '100%'
+  },
+  texto: {
+    fontSize: 30,
+    paddingLeft: 30,
+    paddingTop: 10,
+  },
+  logo: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  logoImg: {
+    width: 200,
+    height: 200,
   }
 });
