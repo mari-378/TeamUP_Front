b20eebd10ca05095be4fd00944218ef194f57b5a
lelepton <leticyammedeiros@gmail.com>
2025-10-01 21:48:51 -0300

    feat: adiciona página de seleção de funcionalidades

---
+++ TeamUP_Front/pages/PaginaDeFuncionalidades.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { useTheme } from "../contexts/ThemeContext";
+import { useTranslation } from 'react-i18next';
+import { View, StyleSheet, Text } from "react-native";
+import SelecaoFuncionalidade from '../components/SelecaoFuncionalidade';
+
+export default function PaginaDeFuncionalidades() {
+    const { temaAtual } = useTheme();
+    const { t } = useTranslation();
+
+    return (
+        <View style={[styles.container, { backgroundColor: temaAtual.fundo }]}>
+            <Text style={[styles.texto, { color: temaAtual.texto }]}>{t('featureOptions.title')}</Text>
+            <SelecaoFuncionalidade />
+        </View>
+    )
+};
+
+const styles = StyleSheet.create({
+    container: {
+        gap: 20,
+    }
+});
